3826ec5b41bc7cafcedc3ba7af1de174bdfd721c
Ivan Pozhidaev <ivan.pozhidaev@gmail.com>
2021-09-20 17:33:12 +0700

    Second commit

--- src/main/java/ru/ivanpozhidaev/App.java
+++ src/main/java/ru/ivanpozhidaev/App.java
@@ -1,13 +1,15 @@
 package ru.ivanpozhidaev;
 
 /**
- * Hello world!
+ * PDFPortfolio can make a portfolio in PDF file, that will contain a wide range of file types
+ * created in different applications.
+ * For example, it can include any MS Office documents and images.
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "It will be my copy of Adobe Acrobat Tool" );
     }
 }
