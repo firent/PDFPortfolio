a6273f645c3db06ce5bb14bafe84797d3dae4f84
Ivan Pozhidaev <ivan.pozhidaev@gmail.com>
2021-10-13 12:58:57 +0700

    With choosing src directory

--- src/main/java/ru/ivanpozhidaev/App.java
+++ src/main/java/ru/ivanpozhidaev/App.java
@@ -7,9 +7,14 @@ import com.itextpdf.kernel.pdf.filespec.PdfFileSpec;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
 
+import javax.swing.*;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * PDFPortfolio can make a portfolio in PDF file, that will contain a wide range of file types
@@ -17,26 +22,69 @@ import java.util.Scanner;
  * For example, it can include any MS Office documents and images.
  *
  */
-public class App 
+public class App extends JFrame
 {
+    private static final long serialVersionUID = 1L;
+
     public static final String DEST = "target/Result.pdf";
-    public static final String DIR = "src/";
 
-    public static final ArrayList<String> list = new ArrayList<>();
+    private static String SRC;
 
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+    private static List<Path> list = new ArrayList<>();
 
-        System.out.println("Enter filenames from SRC directory for portfolio:");
+    private final JButton btnOpenDir;
 
-        while(true){
-            String filename = scanner.next();
-            if (filename.equalsIgnoreCase("exit"))
-                break;
-            list.add(DIR + filename);
-        }
+    private final JFileChooser jFileChooser;
 
-        new App().manipulatePdf();
+    public App() {
+        super("PDFPortfolio");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //Button for choosing a file directory
+        btnOpenDir = new JButton("Открыть директорию файлов для портфолио");
+        // Create JFileChooser instance
+        jFileChooser = new JFileChooser();
+        // Adding listeners to buttons
+        addFileChooserListeners();
+        // Layout of buttons in the interface
+        JPanel contents = new JPanel();
+        contents.add(btnOpenDir);
+        setContentPane(contents);
+        // Window
+        setSize(360, 110);
+        setDefaultLookAndFeelDecorated(true);
+        setLocation(600,300);
+        setVisible(true);
+    }
+
+    private void addFileChooserListeners()
+    {
+        btnOpenDir.addActionListener(e -> {
+            jFileChooser.setDialogTitle("Выбор директории");
+            // Directories only
+            jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            int result = jFileChooser.showOpenDialog(App.this);
+            // If directory selected
+            if (result == JFileChooser.APPROVE_OPTION ) {
+                try {
+                    SRC = jFileChooser.getSelectedFile().getAbsolutePath();
+                    // Adding paths of the all files in selected directory
+                    list = Files.walk(Paths.get(SRC))
+                            .filter(Files::isRegularFile)
+                            .collect(Collectors.toList());
+                    // Creating new PDFPortfolio
+                    new App().manipulatePdf();
+                } catch (Exception ioException) {
+                    ioException.printStackTrace();
+                }
+                // Message about selected directory
+                JOptionPane.showMessageDialog(App.this,
+                        jFileChooser.getSelectedFile());
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new App();
     }
 
     protected void manipulatePdf() throws Exception {
@@ -49,8 +97,9 @@ public class App
         collection.setView(PdfCollection.TILE);
         pdfDoc.getCatalog().setCollection(collection);
 
-        for (String s:list){
-            addFileAttachment(pdfDoc, s, s);
+        for (Path s:list){
+            String str = s.toString();
+            addFileAttachment(pdfDoc, str, str);
         }
 
         doc.close();
